b4c78abdeb5d4bfa926a45001648ac0f80346af4
danieladewale <151259176+danieladewale@users.noreply.github.com>
2025-03-23 04:21:50 -0400

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -6,3 +6,8 @@ export default function App({ Component, pageProps }) {
 import Teams from './components/Teams';
 
 <Route path="/teams" element={<Teams />} />
+
+import CashOut from './components/CashOut';
+
+// In your router configuration
+<Route path="/cashout" element={<CashOut />} />
