90529ed5d6a84fe8eca016d101b20d1fd32fed5c
danieladewale <151259176+danieladewale@users.noreply.github.com>
2025-03-23 04:18:49 -0400

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,3 +3,6 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+import Teams from './components/Teams';
+
+<Route path="/teams" element={<Teams />} />
